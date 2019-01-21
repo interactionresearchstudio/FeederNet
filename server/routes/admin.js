e906b458a1efeb4a61139b195af9b043977e64e9
Mike Vanis <hello@mikevanis.com>
2019-01-21 19:38:00 +0000

    now resolving path to admin

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/*', (req, res, next) => {
     console.log('INFO: GET admin.');
-    res.sendFile(__dirname + '../../../admin-client/build/index.html');
+    res.sendFile(path.resolve('../../../admin-client/build/index.html'));
 });
 
 module.exports = router;
