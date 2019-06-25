ac43c1ce15024cf0588b5182b66b71768e65af06
Mike Vanis <hello@mikevanis.com>
2019-06-25 12:27:51 +0100

    build react

---
+++ admin-client/build/precache-manifest.457085ce8f3fdc207e01e5c25a603fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af2ceb79d7a55ed46bd27a3fd5b88a7",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "d299a160b3e1ed3dec2a",
+    "url": "/admin/static/css/main.f33a86fe.chunk.css"
+  },
+  {
+    "revision": "2ed0004dc0b209aa1ebd",
+    "url": "/admin/static/js/2.220d1bc8.chunk.js"
+  },
+  {
+    "revision": "d299a160b3e1ed3dec2a",
+    "url": "/admin/static/js/main.2137c457.chunk.js"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  }
+]);
