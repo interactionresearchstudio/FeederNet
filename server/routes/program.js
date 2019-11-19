e0e2daacc611db471e3ced325c8d78663876aed5
Mike Vanis <hello@mikevanis.com>
2019-11-19 14:54:20 +0000

    pull bin file from github release, into a tmp file and send that to esptool

--- server/routes/program.js
+++ server/routes/program.js
@@ -1,28 +1,68 @@
 var express = require('express');
 var router = express.Router();
 var esptool = require('esptool-wrapper');
+var request = require('request');
+var tmp = require('tmp');
+var fs = require('fs');
 
 // API routes
-router.get('/program', program);
+router.get('/program', getTags, saveBinary);
 
-// Get all events
-function program(req, res) {
+// Get available tags from GitHub repository.
+function getTags(req, res, next) {
+  axios.get('https://api.github.com/repos/interactionresearchstudio/RFIDBirdFeeder/releases/latest')
+  .then(response => {
+    console.log("Latest release tag: " + response.data.tag_name + " | Asset URL: " + response.data.assets[0].browser_download_url);
+    res.locals.release = response.data;
+    next();
+  })
+  .catch(error => {
+    console.log("ERROR: Could not retrieve tags from firmware repo.");
+    console.log(error);
+    res.sendStatus(500);
+  });
+}
+
+// Check ESP version and send binary file to esptool.
+function sendBinary(req, res, next) {
+  var downloadUrl = res.locals.release.assets[0].browser_download_url;
+  tmp.file((err, path, fd) => {
+    if (err) {
+      console.log("ERROR: Could not create temporary file for binary.");
+      re.sendStatus(500);
+    }
+    console.log("File path: " + path);
+    res.locals.binaryPath = path;
+    var stream = request.get(downloadUrl).pipe(fs.createWriteStream(path));
+    stream.on('finish', () => {
+      program(path, '/dev/ttyUSB0', (err) => {
+        if (err) {
+          console.log("ERROR: esptool error.");
+          console.log(err);
+          res.json({'ERROR': err});
+        }
+        res.json({'SUCCESS': 'Program sent to esp8266 successfully'});
+      });
+    });
+  });
+}
+
+// Program device with firmware
+function program(path, port, callback) {
   console.log("Attempting programming...");
   esptool({
     chip: 'esp8266',
-    port: '/dev/ttyUSB0',
+    port: port,
     baud: 115200,
     files: {
-      0x00000: __dirname + '/blink.bin'
+      0x00000: path
     }
   }, (err) => {
     if (err) {
-      console.log("ERROR!!!");
-      console.log(err);
-      res.json({'ERROR': err});
+      callback(err);
     }
-    console.log("Programming successful.");
-    res.json({'SUCCESS': "Programming successful."});
+    console.log("INFO: Programming successful.");
+    callback();
   });
 }
 
