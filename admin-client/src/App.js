43b6db70566ac30e56149cfb5b1d13c4a10155e3
Mike Vanis <hello@mikevanis.com>
2020-04-03 14:22:09 +0100

    install bootstrap css via npm rather than use cdn

--- admin-client/src/App.js
+++ admin-client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Index from './views/index';
 import LoginForm from './views/login-form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
